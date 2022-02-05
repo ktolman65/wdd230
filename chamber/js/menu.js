4237ddf1b21dcf2467b67d578bbedcf176c77e28
ktolman65 <tol19008@byui.edu>
2022-02-05 14:04:15 -0700

    commit

--- chamber/js/menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-	document.getElementById('topnav').classList.toggle('hide');
-}
-function toggleOff() {
-	if (window.innerWidth > 500) {
-		document.getElementById('topnav').classList.remove('hide');
-	}
-}
--- js/currentDates.js
+++
@@ -1,6 +0,0 @@
-// JavaScript to place last update information in the footer
-document.getElementById('updated').innerHTML = document.lastModified;
-
-// JavaScript to maintain the current year information of copyright in footer
-var currentYear = new Date();
-document.getElementById('currentYear').innerHTML = currentYear.getFullYear();

8f87f52fc934284a2d19afff5a554873f1bb68aa
ktolman65 <tol19008@byui.edu>
2022-02-21 13:24:29 -0700

    major upload to corrct sync errors

---
+++ chamber/js/currentDates.js
@@ -0,0 +1,40 @@
+//declare variables and get date information
+const d = new Date();
+const year = d.getFullYear();
+
+// using getElementById
+document.querySelector("#currentyear").textContent = year;
+document.getElementById("lastupdated").textContent = `Last Updated: ${document.lastModified}`;
+
+// element to output to
+const datefieldUK = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"}).format(now);
+// long, medium, short options ... try them
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+console.log(d.getDay());
+if(d.getDay() === 2 || d.getDay() === 1) {
+	const banner = document.querySelector("#banner");
+	banner.style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+/*
+// JavaScript to place last update information in the footer
+document.getElementById('updated').innerHTML = document.lastModified;
+
+// JavaScript to maintain the current year information of copyright in footer
+var currentYear = new Date();
+document.getElementById('currentYear').innerHTML = currentYear.getFullYear();
+*/

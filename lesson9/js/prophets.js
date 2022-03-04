9ac3e89b0e93fed9c1a6f500ab57f6920ac6c43e
ktolman65 <tol19008@byui.edu>
2022-03-04 08:06:16 -0700

    Mar-4

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,34 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (jsonObject) {
+		const prophets = jsonObject['prophets'];
+		for (let i = 0; i < prophets.length; i++) {
+
+			let card = document.createElement('section');
+			let h2 = document.createElement('h2');
+			let bd = document.createElement('p');
+			let bp = document.createElement('p');
+			let image = document.createElement('img');
+
+			h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
+
+			bd.textContent = `Date of Birth: ${prophets[i].birthdate}`;
+
+			bp.textContent = `Place of Birth: ${prophets[i].birthplace}`;
+
+			image.setAttribute('src', prophets[i].imageurl);
+			image.setAttribute('alt', `Portrait of ${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}`); // Latter-day Prophet
+			image.setAttribute('title', `Portrait of ${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}`); // Latter-day Prophet
+
+			card.appendChild(h2);
+     	card.appendChild(bd);
+			card.appendChild(bp);
+			card.appendChild(image);
+
+			document.querySelector('div.cards').appendChild(card);
+		}
+	});

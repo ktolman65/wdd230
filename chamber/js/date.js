488397b89e656ec589b3b4baf253792c1149e27a
ktolman65 <tol19008@byui.edu>
2022-03-05 21:07:37 -0700

    Mar 5 - #2

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -2,7 +2,6 @@
 const d = new Date();
 const year = d.getFullYear();
 
-
 // using getElementById
 document.querySelector("#currentyear").textContent = year;
 document.getElementById("lastupdated").textContent = `Last Modification: ${document.lastModified}`;
